ffbd4371663168351fb6c605116e0be1304a60b7
Nyan Lin Tun <nyanlynntunn@gmail.com>
2020-05-11 00:50:49 +0630

    Set require UI setting login view

--- On The Map/Controller/LoginViewController.swift
+++ On The Map/Controller/LoginViewController.swift
@@ -9,6 +9,10 @@
 import UIKit
 
 class LoginViewController: UIViewController {
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,14 +21,11 @@ class LoginViewController: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func loginAction(_ sender: UIButton) {
+        
     }
-    */
-
+    
+    @IBAction func signUpAction(_ sender: UIButton) {
+        
+    }
 }
