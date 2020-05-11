1a478d4e27b8b9aad340b7bb73fd28b4da5f9414
Nyan Lin Tun <nyanlynntunn@gmail.com>
2020-05-11 12:44:18 +0630

    Design Map Table View

---
+++ On The Map/Controller/LocationTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  LocationTableViewCell.swift
+//  On The Map
+//
+//  Created by Nyan Lin Tun on 11/05/2020.
+//  Copyright © 2020 Nyan Lin Tun. All rights reserved.
+//
+
+import UIKit
+
+class LocationTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var studentName: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- On The Map/Controller/LocationTableViewController.swift
+++ On The Map/Controller/LocationTableViewController.swift
@@ -13,34 +13,27 @@ class LocationTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
 
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return 4
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let locationCell = tableView.dequeueReusableCell(withIdentifier: "locationTableViewCell", for: indexPath) as! LocationTableViewCell
 
-        // Configure the cell...
+        locationCell.studentName.text = "\(indexPath.row)"
 
-        return cell
+        return locationCell
     }
-    */
+    
 
     /*
     // Override to support conditional editing of the table view.
