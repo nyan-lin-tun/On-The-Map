3d70bbfdbdb94ebb640c30166199fe92df1bedfa
Nyan Lin Tun <nyanlynntunn@gmail.com>
2020-05-11 20:48:21 +0630

    Remove generic post method from network

--- On The Map/Model/APIConstant/OnTheMapClient.swift
+++ On The Map/Model/APIConstant/OnTheMapClient.swift
@@ -59,41 +59,4 @@ class OnTheMapClient {
             
     }
     
-    class func doPOSTRequest<RequestType: Encodable, ResponseType: Decodable> (url: URL, body: RequestType ,responseType: ResponseType.Type, completion: @escaping (ResponseType?, Error?) -> Void){
-        
-        var request = URLRequest(url: url)
-        request.httpMethod = "POST"
-        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-        request.httpBody = try! JSONEncoder().encode(body)
-        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
-            guard let data = data else {
-                DispatchQueue.main.async {
-                    completion(nil, error)
-                }
-                return
-            }
-            
-            let decoder = JSONDecoder()
-            do {
-                let responseObject = try decoder.decode(ResponseType.self, from: data)
-                DispatchQueue.main.async {
-                    completion(responseObject, nil)
-                }
-            }catch {
-                /*
-                do {
-                    let errorResponse = try decoder.decode(TMDBResponse.self, from: data)
-                    DispatchQueue.main.async {
-                        completion(nil, errorResponse)
-                    }
-                }catch {
-                    DispatchQueue.main.async {
-                        completion(nil, error)
-                    }
-                }
- */
-            }
-        }
-        task.resume()
-    }
 }
